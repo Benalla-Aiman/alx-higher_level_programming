16b6aeef7ce005d142aa8955e191a42ee1cd617c
Benalla-Aiman <benallaaiman@gmailcom>
2023-06-14 14:35:51 -0700

    this is the task number 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-# 0-square_matrix_simple.py
-
 def square_matrix_simple(matrix=[]):
-    """Returns a new matrix with the square of each value in the input matrix."""
     new_matrix = matrix.copy()
+
     for i in range(len(matrix)):
         new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
-    return new_matrix
+
+    return (new_matrix)
