32e0f2ba5eed15ead3151a3dba74ec0d944d28cd
Benalla-Aiman <benallaaiman@gmailcom>
2023-06-27 11:28:44 -0700

    this is the task 01 of 0x06-python-classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+
+class Square:
+    """Class that represents a square shape."""
+    
+    def __init__(self, size):
+        """Initialize a new Square object with a given size.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
