2220da512b5d142c94ba78a85f570eed3bbb54ab
Benalla-Aiman <benallaaiman@gmailcom>
2023-07-11 14:39:59 -0700

    this is the task number 4 of 0x0B

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,7 @@
-4-from_json_string.py
 #!/usr/bin/python3
-# 6-from_json_string.py
+# 4-from_json_string.py
 """Defines a JSON-to-object function."""
 import json
-
 
 def from_json_string(my_str):
     """Return the Python object representation of a JSON string."""
